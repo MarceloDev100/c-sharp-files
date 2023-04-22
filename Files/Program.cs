7e64c2b712b76118562123ee992057b80751c777
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-22 20:12:56 -0300

    FileStream and StreamReader

--- Files/Program.cs
+++ Files/Program.cs
@@ -4,24 +4,110 @@
     {
         static void Main(string[] args)
         {
-            string sourcePath = @"C:\Users\marce\Desktop\Files\Files\file1.txt";
-            string targetPath = @"C:\Users\marce\Desktop\Files\Files\file2.txt";
+            /*
+             *   Stream is a a data flow. It's common to transmit data sequence.
+             * 
+              ****** FileStream ****** 
+              *
+              -Provides a stream associated with a file allowing reading and writing operations.
+              -Binary data support
+              Instantiation:
+              - Many constructors
+              - File/ FileInfo
 
+              ****** StreamReader ****** 
+              *
+              - It's a stream capable of reading characters from a binary stream(ex: FileStream).
+              - Data support in text format.
+              Instantiation:
+              - Many constructors.
+              - File/ FileInfo
+             */
+
+            string path = @"c:\temp\file1.txt";
+
+            ReadFileMode1(path);
+            ReadFileMode2(path);
+            Console.WriteLine("-----------------------");
+            ReadFileAllLines(path);
+        }
+
+        static void ReadFileMode1(string path)
+        {
+            FileStream fs = null;
+            StreamReader sr = null;
             try
             {
-                FileInfo fileInfo = new FileInfo(sourcePath);
-                fileInfo.CopyTo(targetPath);
-
-                string[] lines = File.ReadAllLines(sourcePath);
-                foreach (string line in lines)
+                fs = new FileStream(path, FileMode.Open);
+                sr = new StreamReader(fs);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
                 {
+                    sr.Close();
+                }
+
+                if (fs != null)
+                {
+                    fs.Close();
+                }
+            }
+        }
+
+        static void ReadFileMode2(string path)
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(path);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
+            }
+        }
+
+        static void ReadFileAllLines(string path)
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
                     Console.WriteLine(line);
                 }
             }
-            catch(IOException e)
+            catch (IOException e)
             {
-                Console.WriteLine("An error occurred!");
+                Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                {
+                    sr.Close();
+                }
             }
         }
     }
