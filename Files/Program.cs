96f99b299a30c6e09615212c5d56d3462ddba3e3
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-22 20:38:48 -0300

    Using block

--- Files/Program.cs
+++ Files/Program.cs
@@ -5,109 +5,60 @@
         static void Main(string[] args)
         {
             /*
-             *   Stream is a a data flow. It's common to transmit data sequence.
-             * 
-              ****** FileStream ****** 
-              *
-              -Provides a stream associated with a file allowing reading and writing operations.
-              -Binary data support
-              Instantiation:
-              - Many constructors
-              - File/ FileInfo
+               "Using blocks"
 
-              ****** StreamReader ****** 
-              *
-              - It's a stream capable of reading characters from a binary stream(ex: FileStream).
-              - Data support in text format.
-              Instantiation:
-              - Many constructors.
-              - File/ FileInfo
+              -Simplified syntax that garantees that IDisposable objects will be closed.
+              -IDisposable objects are NOT managed by the CLR. They have to be manually closed.
+              
              */
 
             string path = @"c:\temp\file1.txt";
 
-            ReadFileMode1(path);
-            ReadFileMode2(path);
-            Console.WriteLine("-----------------------");
-            ReadFileAllLines(path);
-        }
+            // Instantiated resources will be automatically closed.
+            // "Using blocks" can be nested.
+            // "Using blocks" will not handle exceptions. If you want to handle it, it must
+            // have a try/catch block.
 
-        static void ReadFileMode1(string path)
-        {
-            FileStream fs = null;
-            StreamReader sr = null;
             try
             {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                {
+
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            Console.WriteLine(line);
+                        }
+                    }
+                }
             }
             catch (IOException e)
             {
-                Console.WriteLine("An error occurred");
+                Console.WriteLine("An error occurred!");
                 Console.WriteLine(e.Message);
             }
-            finally
-            {
-                if (sr != null)
-                {
-                    sr.Close();
-                }
 
-                if (fs != null)
-                {
-                    fs.Close();
-                }
-            }
-        }
+            Console.WriteLine("--------------------------------------------");
 
-        static void ReadFileMode2(string path)
-        {
-            StreamReader sr = null;
+            // Other way instead of using nested blocks 
+
             try
             {
-                sr = File.OpenText(path);
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine("An error occurred");
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if (sr != null)
+                using (StreamReader sr = File.OpenText(path))
                 {
-                    sr.Close();
-                }
-            }
-        }
-
-        static void ReadFileAllLines(string path)
-        {
-            StreamReader sr = null;
-            try
-            {
-                sr = File.OpenText(path);
-                while (!sr.EndOfStream)
-                {
-                    string line = sr.ReadLine();
-                    Console.WriteLine(line);
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
                 }
             }
             catch (IOException e)
             {
                 Console.WriteLine("An error occurred");
                 Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                if (sr != null)
-                {
-                    sr.Close();
-                }
             }
         }
     }
