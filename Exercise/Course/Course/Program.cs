4a32c040fa909186018dcb7bac28eef291a38381
Marcelo Carneiro <marcelo.dev100@gmail.com>
2023-04-23 11:44:29 -0300

    Exercise done

---
+++ Exercise/Course/Course/Program.cs
@@ -0,0 +1,53 @@
+﻿using Course.Entities;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter file full path: ");
+            string sourcePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+                        string[] items = line.Split(",");
+                        string name = items[0];
+                        double price = double.Parse(items[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(items[2]);
+
+                        Product p = new Product(name, price, quantity);
+
+                        sw.Write(p.Name);
+                        sw.Write(",");
+                        sw.WriteLine(p.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred!");
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected exception!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Exercise/Course/README.md
@@ -0,0 +1,3 @@
+## C# .NET6.0 Exercise (Files)
+
+Write a program to read the path of a .csv file containing data on sold items. Each item has a name, unit price and quantity, separated by comma. You must generate a new file called summary.csv, located in a subfolder called "out" from the original file folder, containing only the name and total value for that item (unit price multiplied by quantity). 
